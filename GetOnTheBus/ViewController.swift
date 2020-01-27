32a4dd865b44330c5f6ab08a107c870425eb1fc2
Gerardo Carranza <geraljrluna@gmail.com>
2020-01-27 14:14:33 -0500

    Initial Commit

---
+++ GetOnTheBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetOnTheBus
+//
+//  Created by Gerardo Carranza on 1/27/20.
+//  Copyright © 2020 Gerardo Carranza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
